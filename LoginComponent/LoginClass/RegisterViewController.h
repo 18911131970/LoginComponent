3225b67bb27c061184dbb27ed6ec791997774b2a
yangzhe <385609722@qq.com>
2017-02-16 10:49:22 +0800

    注册页面

---
+++ LoginComponent/LoginClass/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  LoginComponent
+//
+//  Created by yangzhe on 17/2/16.
+//  Copyright © 2017年 yangzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
---
+++ LoginComponent/LoginClass/RegisterViewController.m
@@ -0,0 +1,39 @@
+//
+//  RegisterViewController.m
+//  LoginComponent
+//
+//  Created by yangzhe on 17/2/16.
+//  Copyright © 2017年 yangzhe. All rights reserved.
+//
+
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+
+@end
+
+@implementation RegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"登录";
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
