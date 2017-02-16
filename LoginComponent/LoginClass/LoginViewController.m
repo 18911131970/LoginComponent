a32b6dbaca5108b87c95970b9fd92df335f03efc
yangzhe <385609722@qq.com>
2017-02-16 10:16:06 +0800

    修改为蓝色

--- LoginComponent/LoginClass/LoginViewController.m
+++ LoginComponent/LoginClass/LoginViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"登录";
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = [UIColor blueColor];
 }
 
 
