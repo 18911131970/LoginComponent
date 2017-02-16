dcdf74968f11cb3e6978159215617057985d53ad
yangzhe <385609722@qq.com>
2017-02-16 09:36:33 +0800

    init

---
+++ LoginComponent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginComponent
+//
+//  Created by yangzhe on 17/2/15.
+//  Copyright © 2017年 yangzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginComponent/LoginClass/LoginViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LoginComponent
+//
+//  Created by yangzhe on 17/2/15.
+//  Copyright © 2017年 yangzhe. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"登录";
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
